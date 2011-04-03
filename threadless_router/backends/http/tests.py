448177a0b3418579c6ddd957991b9d9e69e4b00f
Colin Copeland <copelco@caktusgroup.com>
2011-04-03 14:21:28 -0400

    rename tests; add get_incoming_data test

--- threadless_router/backends/http/tests.py
+++ threadless_router/backends/http/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase, RequestFactory
 from django.core.urlresolvers import reverse
 
 from threadless_router.backends.http import views
+from threadless_router.backends.http.forms import BaseHttpForm
 
 
 class HttpTest(TestCase):
@@ -20,7 +21,7 @@ class HttpTest(TestCase):
         request = self.rf.post(self.url, data)
         return self.view(request, backend_name='simple-http')
 
-    def testValidForm(self):
+    def test_valid_form(self):
         """ Form should be valid if POST keys match configuration """
         view = views.SimpleHttpBackendView(conf=self.conf)
         data = {'phone': '1112223333', 'message': 'hi there'}
@@ -28,7 +29,7 @@ class HttpTest(TestCase):
         form = view.get_form(view.get_form_class())
         self.assertTrue(form.is_valid())
 
-    def testInvalidForm(self):
+    def test_invalid_form(self):
         """ Form is invalid if POST keys don't match configuration """
         view = views.SimpleHttpBackendView(conf=self.conf)
         data = {'invalid-phone': '1112223333', 'invalid-message': 'hi there'}
@@ -36,8 +37,13 @@ class HttpTest(TestCase):
         form = view.get_form(view.get_form_class())
         self.assertFalse(form.is_valid())
 
-    def testInvalidResponse(self):
+    def test_invalid_response(self):
         """ HTTP 400 should return if form is invalid """
         data = {'invalid-phone': '1112223333', 'message': 'hi there'}
         response = self._post(data)
         self.assertEqual(response.status_code, 400)
+
+    def test_get_incoming_data(self):
+        """ Subclasses must implement get_incoming_data """
+        form = BaseHttpForm()
+        self.assertRaises(NotImplementedError, form.get_incoming_data)
