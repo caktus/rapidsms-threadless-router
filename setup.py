bb27a5aa6fa9df112a72f7e5aaa9a5b99c3b78ac
Mark Lavin <mlavin@caktusgroup.com>
2012-06-28 13:17:30 -0400

    Update version and copyright date.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='threadless_router',
-    version='0.0.1',
+    version='0.1.0',
     author='Caktus Consulting Group',
     author_email='solutions@caktusgroup.com',
     packages=find_packages(),
