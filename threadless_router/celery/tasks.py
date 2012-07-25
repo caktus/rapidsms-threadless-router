2474221a23658a363b607fe4d32a2171a2f65872
Tim Akinbo <takinbo@timbaobjects.com>
2012-07-25 15:30:53 +0100

    remove djcelery from INSTALLED_APPS before routing Removes djcelery from INSTALLED_APPS before starting the router to prevent processing the djcelery app module as a RapidSMS app.

--- threadless_router/celery/tasks.py
+++ threadless_router/celery/tasks.py
@@ -1,4 +1,5 @@
 import datetime
+from django.conf import settings
 
 from rapidsms.models import Connection, Backend
 from rapidsms.messages import IncomingMessage
@@ -14,7 +15,14 @@ class IncomingTask(Task):
         backend, _ = Backend.objects.get_or_create(name=backend_name)
         connection, _ = backend.connection_set.get_or_create(identity=identity)
         message = IncomingMessage(connection, text, datetime.datetime.now())
-        router = Router()
+        # remove the djcelery app from the INSTALLED_APPS list
+        # to prevent the router from attempting to process messages
+        # through the app module of djcelery
+        try:
+            settings.INSTALLED_APPS.remove('djcelery')
+        except ValueError:
+            pass
+        router = Router(apps=settings.INSTALLED_APPS)
         response = router.incoming(message)
 
 
